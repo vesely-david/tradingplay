f919f62261b242c766856a152788e28b0a9d689e
vesely-david <veselda7@gmail.com>
2021-12-21 21:53:48 +0100

    Converts czk to usd

--- convertor/yahoo_convertor.py
+++ convertor/yahoo_convertor.py
@@ -5,12 +5,18 @@ import yfinance as yf
 
 
 class YahooConvertor:
-    def __init__(self, ticker: str):
+    def __init__(self, ticker: str, inverse: bool = False):
         self._ticker = ticker
+        self._inverse = inverse
 
     def convert(self, value: float, date_: date):
         ten_days = timedelta(days=10)
         _date_min = (date_ - ten_days).strftime('%Y-%m-%d')
         _date_max = (date_ + ten_days).strftime('%Y-%m-%d')
         prices = yf.download(self._ticker, start=_date_min, end=_date_max)
-        return prices.loc[Timestamp(date_), "Close"] * value
+        price = round(prices.loc[Timestamp(date_), "Close"], 3)
+
+        if self._inverse:
+            return value * (1 / price)
+        else:
+            return value * price
--- tests/test_convertor.py
+++ tests/test_convertor.py
@@ -17,3 +17,15 @@ def test_converts_usd_to_czk(value, expected):
     converted = convertor.convert(value, date(2021, 12, 21))
     assert converted == pytest.approx(expected, 0.001)
 
+
+@pytest.mark.parametrize(
+    'value,expected',
+    [
+        (1.0, 1/22.3430),
+        (10, 10/22.3430)
+    ]
+)
+def test_converts_czk_to_usd(value, expected):
+    convertor = YahooConvertor(ticker='CZK=X', inverse=True)
+    converted = convertor.convert(value, date(2021, 12, 21))
+    assert converted == pytest.approx(expected, 0.001)
